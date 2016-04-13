54a6eb7ddbf74e8b9f3675717a5a0f1e27bbca10
Gonçalo Amaral <Gonalo Amaral>
2016-04-13 22:53:32 +0100

    Criaçao da Suggestions var = sugestoes

--- Scraper.rb
+++ Scraper.rb
@@ -1,10 +1,34 @@
 require 'HTTParty'
 require 'Nokogiri'
-require 'JSON'
 require 'Pry'
-require 'csv'
 
 page = HTTParty.get('http://oquefaltaemcoimbra.pt/')
 
-Pry.start(binding)
+parse_page = Nokogiri::HTML(page)
 
+filtered = []
+
+#Filter suggestions
+parse_page.css('.suggestion-text').map do |a|
+    suggesttion = a.text 
+    filtered.push(suggesttion)
+end
+
+aux = []
+
+filtered.each do |line|
+    nl =""
+    line.each_char do |char|
+       if char != "\n"
+            nl+=char
+       else
+           break 
+       end
+        
+    end
+    aux.push(nl)
+end
+
+suggestions = aux
+
+Pry.start(binding)  
