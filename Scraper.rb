2e120c4d2a4f63ab451259aae7b5da4c19021ff5
Gonçalo Amaral <Gonalo Amaral>
2016-04-14 20:11:49 +0100

    Finished utf fix

--- Scraper.rb
+++ Scraper.rb
@@ -52,27 +52,73 @@ suggestions.each do |line|
     aux += line + " " 
 end
 
-#Need to remove symbols like ",","!","#"...
-
+#Remove symbols
 symb=",;.:_~^ºª´`+*«»'?}=])[({/&%$§#£@!\"\\<>"
 
 aux = clean_str(aux,symb)
 
+#downcase
+text= aux.downcase
 
-aux = aux.split(" ")
-words = Hash.new
+simplify={
+    "ç"=>"c",
+    "é"=>"e",
+    "è"=>"e",
+    "ê"=>"e",
+    "ã"=>"a",
+    "â"=>"a",
+    "á"=>"a",
+    "à"=>"a",
+    "î"=>"i",
+    "í"=>"i",
+    "ì"=>"e",
+    "õ"=>"o",
+    "ô"=>"o",
+    "ó"=>"o",
+    "ò"=>"o",
+    "û"=>"u",
+    "ú"=>"u",
+    "ù"=>"u",
+    }
 
-aux.each do |word|
-    value = words[word]
-    if value != nil
-        words[word]+=1
-    else
-        words[word]=1
+aux=""
+
+text.each_char do |char|
+    if simplify.has_key?(char)
+        char = simplify[char]
+    end    
+    aux+=char
+end
+
+text= aux
+
+    
+#remove all spaces
+text = text.split(" ")
+
+    words = text
+
+#Create Hash with all the words without prepositions
+words_freq = Hash.new
+
+prepositions = ["de","e","a","o","da","para","que","na","em","com","do","mais","um","uma","ao","os","no","ou","as","dos","nas","como","das","se","por","aos","sem","nos","ser","falta","ter","entre","lado","menos","nao","é"]
+
+words.each do |word|
+    value = words_freq[word]
+    if (prepositions.include? word) != true && value != nil
+        words_freq[word]+=1
+    #exclude prepositions
+        else if value == nil
+        words_freq[word]=1 
+        end
     end
 end
 
-words_freq = words.sort_by {|key,value|value}
+words_freq = words_freq.sort_by {|key,value|value}.to_h
 
-words_freq = words_freq.reverse!.to_h
+#words_freq = words_freq.reverse!.to_h
 
-Pry.start(binding) 
+words_freq.each do |key,value|
+    puts "#{key}: #{value}" 
+end
+#Pry.start(binding)
